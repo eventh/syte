0d40d55fc89a6a7cee42679bf0eef30e1716ca82
Even Wiik Thomassen <eventh@gmail.com>
2012-08-08 01:25:18 +0200

    Change compress.py to use r.js from requirejs.

--- syte/compress.py
+++ syte/compress.py
@@ -1,101 +1,84 @@
-
 import os
 import sys
 import subprocess
 import shlex
-import traceback
 
-path_to_here = os.path.abspath(os.path.dirname(__file__))
-path_before_site = path_to_here[0:path_to_here.rfind('syte')]
-sys.path.append(path_before_site)
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+OUT_PATH = os.path.join(PATH, 'static/js/min/')
+sys.path.append(os.path.join(PATH, '..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'syte.settings'
+
 
 from django.conf import settings
 
+
 def compress_statics():
+    """Compress both CSS and JavaScript files."""
     try:
-        #This won't work on windows.
-        subprocess.check_call(shlex.split('mkdir -p static/css static/js/min'))
-    except Exception:
-        print 'Make sure to create "syte > static > css" and "syte > static > js > min" before compressing statics.'
+        for path in (os.path.join(PATH, 'static/css'), OUT_PATH):
+            if not os.path.exists(path):
+                os.mkdir(path)
+    except OSError:
+        print 'Make sure to create "syte > static > css" and ' \
+                '"syte > static > js > min" before compressing statics.'
 
     compress_styles()
     compress_js()
 
-def compress_styles():
-    less_path = 'static/less/styles.less'
-    css_path = 'static/css/'
 
-    try:
-        subprocess.check_call(shlex.split('lessc {0} {1}styles-{2}.min.css -yui-compress'
-            .format(less_path, css_path, settings.COMPRESS_REVISION_NUMBER)))
-        print 'CSS Styles Generated: styles-{0}.min.css'.format(settings.COMPRESS_REVISION_NUMBER)
-    except Exception:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        stack_trace = traceback.format_exception(exc_type, exc_value, exc_traceback)
-        print stack_trace
+def compress_styles():
+    less_path = os.path.join(PATH, 'static/less/styles.less')
+    css_path = os.path.join(PATH, 'static/css/')
+
+    subprocess.check_call(
+        shlex.split('lessc {0} {1}styles-{2}.min.css -yui-compress'.format(
+            less_path, css_path, settings.COMPRESS_REVISION_NUMBER)))
+
+    print 'CSS Styles Generated: {0}styles-{1}.min.css'.format(
+        css_path, settings.COMPRESS_REVISION_NUMBER)
+
 
 def compress_js():
-    js_files = [
-      'libs/jquery.url.js',
-      'libs/require.js',
-      'libs/handlebars.js',
-      'libs/moment.min.js',
-      'libs/bootstrap-modal.js',
-      'libs/spin.min.js',
-      'libs/prettify.js',
+    js_files = (
+        ('js/libs/handlebars', True),
+        ('js/libs/moment', True),
+        ('js/libs/bootstrap-modal', True),
+        ('js/libs/spin', True),
+        ('js/libs/prettify', True),
+        ('js/components/blog-posts', True),
+        ('js/components/mobile', True),
+        ('js/components/links', True),
+        ('js/components/twitter', settings.TWITTER_INTEGRATION_ENABLED),
+        ('js/components/dribbble', settings.DRIBBBLE_INTEGRATION_ENABLED),
+        ('js/components/github', settings.GITHUB_INTEGRATION_ENABLED),
+        ('js/components/instagram', settings.INSTAGRAM_INTEGRATION_ENABLED),
+        ('js/components/disqus', settings.DISQUS_INTEGRATION_ENABLED),
+        ('js/components/lastfm', settings.LASTFM_INTEGRATION_ENABLED),
+        ('js/components/bitbucket', settings.BITBUCKET_INTEGRATION_ENABLED),
+        ('js/components/soundcloud', settings.SOUNDCLOUD_INTEGRATION_ENABLED),
+        ('js/components/ohloh', settings.OHLOH_INTEGRATION_ENABLED),
+    )
 
-      'components/base.js',
-      'components/mobile.js',
-      'components/blog-posts.js',
-      'components/links.js',
-    ]
+    includes = ','.join(path for path, include in js_files if include)
 
-    if settings.TWITTER_INTEGRATION_ENABLED:
-        js_files.append('components/twitter.js')
+    subprocess.check_call(shlex.split(
+        'r.js -o baseUrl={0}/static/ name=js/components/base include={1}'
+        ' mainConfigFile={0}/static/js/components/base.js paths.jquery=empty:'
+        ' out={2}scripts-{3}.min.js'.format(
+            PATH, includes, OUT_PATH, settings.COMPRESS_REVISION_NUMBER)))
 
-    if settings.GITHUB_INTEGRATION_ENABLED:
-        js_files.append('components/github.js')
+    print 'JavaScript Combined and Minified: {0}scripts-{1}.min.js'.format(
+        OUT_PATH, settings.COMPRESS_REVISION_NUMBER)
 
-    if settings.DRIBBBLE_INTEGRATION_ENABLED:
-        js_files.append('components/dribbble.js')
+    # Minify require.js
+    # TODO: include require.js above, and use jquery from google CDN!!
+    subprocess.check_call(shlex.split(
+        'uglifyjs -o {0}require.min.js {1}/static/js/libs/require.js'.format(
+            OUT_PATH, PATH)))
 
-    if settings.INSTAGRAM_INTEGRATION_ENABLED:
-        js_files.append('components/instagram.js')
+    print 'require.js Minified: {0}require.min.js'.format(OUT_PATH)
 
-    if settings.DISQUS_INTEGRATION_ENABLED:
-        js_files.append('components/disqus.js')
-
-    if settings.LASTFM_INTEGRATION_ENABLED:
-        js_files.append('components/lastfm.js')
-
-    if settings.SOUNDCLOUD_INTEGRATION_ENABLED:
-        js_files.append('components/soundcloud.js')
-
-    if settings.BITBUCKET_INTEGRATION_ENABLED:
-        js_files.append('components/bitbucket.js')
-
-    if settings.OHLOH_INTEGRATION_ENABLED:
-        js_files.append('components/ohloh.js')
-
-    combined = ''
-    for js in js_files:
-        f = open('static/js/' + js, 'r')
-        combined += f.read()
-        f.close()
-
-    f = open('static/js/combined.js', 'w')
-    f.write(combined)
-    f.close()
-
-    try:
-        subprocess.check_call(shlex.split('uglifyjs -o static/js/min/scripts-{0}.min.js static/js/combined.js'.format(settings.COMPRESS_REVISION_NUMBER)))
-        subprocess.check_call(shlex.split('rm -f static/js/combined.js'))
-        print 'JavaScript Combined and Minified: scripts-{0}.min.js'.format(settings.COMPRESS_REVISION_NUMBER)
-    except Exception:
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        stack_trace = traceback.format_exception(exc_type, exc_value, exc_traceback)
-        print stack_trace
 
 if __name__ == "__main__":
     compress_statics()
--- syte/static/js/components/base.js
+++ syte/static/js/components/base.js
@@ -1,18 +1,17 @@
-//Global configs and functions shared between js
-
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
+// Global configs and functions shared between js
+// Require.js configs
 require.config({
-  baseUrl: "/static/",
   paths: {
     "text": "js/libs/text",
-    "json": "js/libs/json"
+    "json": "js/libs/json",
   },
-  waitSeconds: 15
+  optimizeAllPluginResources: true,
+  preserveLicenseComments: false,
+  inlineText: true,
+  inlineJSON: false,
 });
 
+// Spin.js configs
 var spin_opts = {
   lines: 9,
   length: 5,
@@ -27,3 +26,63 @@ var spin_opts = {
   className: 'spinner',
   zIndex: 2e9
 };
+
+// index.html --- Load blog posts
+require(["jquery", "js/components/blog-posts", "js/libs/jquery.url", "json"],
+  function() {
+    $(function() {
+      if (typeof indexPage === 'undefined' || !indexPage) {
+        return
+      }
+
+      if (tagSlug) {
+        fetchBlogPosts(postOffset, tagSlug);
+      } else {
+        fetchBlogPosts(postOffset);
+      }
+      if (disqus_integration_enabled) {
+        $('body').bind('blog-post-loaded', function() {
+          embedDisqus(true);
+        });
+      }
+    });
+
+    var resultsLoaded = false;
+    $(window).scroll(function() {
+      if (!resultsLoaded && ($(window).scrollTop() + $(window).height() > $(document).height()/1.2)) {
+        resultsLoaded = true;
+        postOffset += 20;
+        if (tagSlug) {
+          fetchBlogPosts(postOffset, tagSlug);
+        } else {
+          fetchBlogPosts(postOffset);
+        }
+      }
+      if (resultsLoaded && ($(window).scrollTop() + $(window).height() < $(document).height()/1.2)) {
+        resultsLoaded = false;
+      }
+    });
+  }
+);
+
+// blog-posts.html --- load a single blog post
+require(["jquery", "js/components/links", "js/components/blog-posts"],
+  function(base) {
+    $(function() {
+      if (typeof blogsPage === 'undefined' || !blogsPage) {
+        return
+      }
+
+      setupLinks();
+      adjustBlogHeaders();
+
+      if (disqus_integration_enabled) {
+        embedDisqus();
+      }
+    });
+  }
+);
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
