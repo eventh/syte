6b10dc1844034f6d798927a5c5cd907efe07bbef
Even Wiik Thomassen <eventh@gmail.com>
2012-08-17 17:37:57 +0200

    Update with modifications I did to ohloh-integration branch.

--- syte/views/ohloh.py
+++ syte/views/ohloh.py
@@ -21,6 +21,8 @@ def ohloh(request, username, refresh=False):
         settings.OHLOH_API_URL, username, settings.OHLOH_API_KEY))
 
     root = ElementTree.fromstring(r.text)
+
+    # Error handling
     error = root.find('error')
     if error is not None:
         print "Ohloh error:", error.text
@@ -31,16 +33,15 @@ def ohloh(request, username, refresh=False):
     context = {node.tag: node.text for node in account}
     context['position'] = account.find('kudo_score/position').text
     context['kudo_score'] = account.find('kudo_score/kudo_rank').text
-    context['monthly_commits'] = 0
-    context['total_commits'] = 0
-    ratio = 0.0
-    ratio_count = 0
 
     # Accounts language experiences
+    total_commits = 0
+    total_lines = 0
     context['languages'] = []
     for node in account.find('languages'):
         values = {i.tag: i.text for i in node}
 
+        # Language experience prettify output
         years = int(values['experience_months']) // 12
         months = int(values['experience_months']) % 12
         if years:
@@ -51,16 +52,12 @@ def ohloh(request, username, refresh=False):
         values['name'] = values['name'].capitalize()
         context['languages'].append(values)
 
-        context['monthly_commits'] += int(values['median_monthly_commits'])
-        #context['total_commits'] += int(values['total_commits'])
-
-        # Average comment ratio stat
-        if values['comment_ratio'] != '-':
-            ratio += float(values['comment_ratio'].replace('%', ''))
-            ratio_count += 1
+        #total_commits += int(values['total_commits'].replace(',', ''))
+        total_lines += int(values['total_lines_changed'].replace(',', ''))
 
     context['total_languages'] = len(context['languages'])
-    context['comment_ratio'] = '%.1f%%' % (ratio / ratio_count)
+    context['total_commits'] = total_commits
+    context['total_lines_changed'] = total_lines
 
     # Project managed by 'username'
     man_r = requests.get('{0}accounts/{1}/projects.xml?api_key={2}'.format(
@@ -88,9 +85,6 @@ def ohloh(request, username, refresh=False):
     for project_id, contrib_id in settings.OHLOH_CONTRIB_IDS:
         project = projects[project_id]
 
-        if not project.get('homepage_url', False):
-            project['homepage_url'] = project['html_url']
-
         # Find contributions tied to our account id
         pro_r = requests.get(
             '{0}projects/{1}/contributors/{2}.xml?api_key={3}'.format(
@@ -115,5 +109,5 @@ def ohloh(request, username, refresh=False):
     if not (r.error or man_r.error):
         cache.set('ohloh', content, 7200)  # 2 hours
 
-    return HttpResponse(content, 'application/json; charset=utf-8',
-                        r.status_code)
+    return HttpResponse(content,
+                        'application/json; charset=utf-8', r.status_code)
