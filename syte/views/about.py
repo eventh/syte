39884f998e66dc1cedfd63f55b8def528b035c73
Even Wiik Thomassen <eventh@gmail.com>
2012-08-19 01:29:58 +0200

    Update with latest changes.

--- syte/views/about.py
+++ syte/views/about.py
@@ -3,7 +3,16 @@ import json
 
 import requests
 import django
+import boto
+import rauth
+import storages
+import gunicorn
+import greenlet
+import gevent
+import pylibmc
+import django_pylibmc
 from django.http import HttpResponse
+from django.conf import settings
 
 
 # List of packages used by Syte
@@ -19,54 +28,121 @@ BASE_PACKAGES = [
     {'name': 'Requests',
      'url': '//python-requests.org/',
      'version': requests.__version__,
-     'description': 'Copyright (c) 2012 Kenneth Reitz',
+     'description': 'Copyright (c) 2012 Kenneth Reitz.',
      'license': 'ISC'},
+
+    {'name': 'jQuery',
+     'url': '//jquery.com/',
+     'version': '1.8.0',
+     'description': 'Copyright 2012 jQuery Foundation and other contributors.',
+     'license': 'MIT'},
 
     {'name': 'RequireJS',
      'url': '//github.com/jrburke/requirejs',
      'version': '1.0.8',
-     'description': 'Copyright (c) 2010-2012, The Dojo Foundation '
-     'All Rights Reserved',
+     'description': 'Copyright (c) 2010-2012, The Dojo Foundation.',
+     'license': 'MIT'},
+
+    {'name': 'RequireJS JSON plugin',
+     'url': '//github.com/millermedeiros/requirejs-plugins',
+     'version': '0.2.1',
+     'description': 'Author: Miller Medeiros.',
      'license': 'MIT'},
 
     {'name': 'handlebars',
      'url': '//handlebarsjs.com/',
      'version': '1.0.beta.6',
-     'description': 'Copyright (C) 2011 by Yehuda Katz',
+     'description': 'Copyright (C) 2011 by Yehuda Katz.',
      'license': 'MIT'},
 
     {'name': 'Moment.js',
      'url': '//momentjs.com/',
      'version': '1.6.2',
-     'description': 'Copyright (c) 2011-2012 Tim Wood',
+     'description': 'Copyright (c) 2011-2012 Tim Wood.',
      'license': 'MIT'},
 
     {'name': 'spin.js',
      'url': '//fgnass.github.com/spin.js/',
      'version': '1.2.5',
-     'description': 'Copyright (c) 2011 Felix Gnass [fgnass at neteye dot de]',
+     'description': 'Copyright (c) 2011 Felix Gnass.',
      'license': 'MIT'},
 
     {'name': 'bootstrap-modal.js',
      'url': '//twitter.github.com/bootstrap/javascript.html#modals',
      'version': '2.0.3',
-     'description': 'Copyright 2012 Twitter, Inc',
+     'description': 'Copyright 2012 Twitter, Inc.',
      'license': 'Apache Version 2.0'},
 
     {'name': 'jQuery URL Parser',
      'url': '//github.com/allmarkedup/jQuery-URL-Parser',
-     'description': 'Copyright (c) 2012 Mark Perkins, http://allmarkedup.com/',
+     'description': 'Copyright (c) 2012 Mark Perkins, '
+     'http://allmarkedup.com/.',
      'license': 'MIT'},
 
     {'name': 'google-code-prettify',
      'url': '//code.google.com/p/google-code-prettify/',
-     'description': 'Copyright (C) 2006 Google Inc',
+     'description': 'Copyright (C) 2006 Google Inc.',
      'license': 'Apache Version 2.0'},
+
+    {'name': 'pybars',
+     'url': '//launchpad.net/pybars',
+     'version': '0.0.2',
+     'description': 'Copyright (c) 2012, Canonical Ltd.',
+     'license': 'LGPLv3'},
+
+    {'name': 'boto',
+     'url': '//github.com/boto/boto',
+     'version': boto.__version__,
+     'license': 'MIT'},
+
+    {'name': 'django-storages',
+     'url': '//django-storages.readthedocs.org/',
+     'version': storages.__version__,
+     'license': 'BSD'},
+
+    {'name': 'gunicorn',
+     'url': '//gunicorn.org/',
+     'version': gunicorn.__version__,
+     'description': '2009,2010 (c) Benoît Chesneau <benoitc@e-engura.org>\n'
+     '2009,2010 (c) Paul J. Davis <paul.joseph.davis@gmail.com>.',
+     'license': 'MIT'},
+
+    {'name': 'greenlet',
+     'url': '//pypi.python.org/pypi/greenlet',
+     'version': greenlet.__version__,
+     'description': 'Copyright (c) Armin Rigo, Christian Tismer and '
+     'contributors.',
+     'license': 'MIT'},
+
+    {'name': 'gevent',
+     'url': '//www.gevent.org/',
+     'version': gevent.__version__,
+     'description': 'Copyrighted by Denis Bilenko and the contributor.',
+     'license': 'MIT'},
+
+    {'name': 'pylibmc',
+     'url': '//sendapatch.se/projects/pylibmc/',
+     'version': pylibmc.__version__,
+     'description': 'Copyright (c) 2008, Ludvig Ericson.',
+     'license': 'BSD'},
+
+    {'name': 'django-pylibmc-sasl',
+     'url': '//pypi.python.org/pypi/django-pylibmc-sasl',
+     'version': django_pylibmc.__version__,
+     'description': 'Copyright (c) 2010, Jeff Balogh.',
+     'license': 'BSD'},
 ]
 
 
 def about(request):
     packages = BASE_PACKAGES[:]
+
+    if settings.TWITTER_INTEGRATION_ENABLED:
+        packages.append({
+            'name': 'rauth', 'url': '//rauth.readthedocs.org/',
+            'version': rauth.__version__, 'license': 'MIT',
+            'description': 'Rauth is Copyright (c) 2012 litl, LLC',
+        })
 
     context = {'count': len(packages), 'packages': packages}
     return HttpResponse(json.dumps(context),
--- syte/views/documents.py
+++ syte/views/documents.py
@@ -9,12 +9,39 @@ from django.conf import settings
 # {file, title, subtitle, description, date, url}
 # Use url to give direct link to file, or file to host yourself.
 ALL_DOCUMENTS = [
-    {'file': 'filename.pdf',
-     'title': 'TODO: document title',
-     'subtitle': 'TODO: add document subtitle',
-     'description': 'TODO: document description',
-     'url': '',
+    {'title': 'Implementing Haskell in RPython',
+     'description': 'PyHaskell is an Haskell VM written in RPython, '
+     'to investigate how Haskell can benefit from JIT compilation '
+     'techniques. Report from specialization project at NTNU.',
+     'url': 'http://bitbucket.org/eventh/courses/downloads/Implementing%20Haskell%20in%20RPython.pdf',
      'date': 'June 2012'},
+
+    {'title': 'Evaluation of the PyPy Project',
+     'subtitle': 'Past, Present and the Future',
+     'description': 'An evaluation of the PyPy project --- the Python intepreter and the RPython translation toolchain.',
+     'url': 'http://bitbucket.org/eventh/courses/downloads/Evaluation%20of%20PyPy%20Project%20-%20Past,%20Present%20and%20Future.pdf',
+     'date': 'February 2012'},
+
+    {'title': 'CSjark',
+     'subtitle': 'Automated Generation of Protocol Dissectors for Wireshark',
+     'description': 'Report from TDT4290 Customer Driven Project at Norwegian at NTNU.',
+     'url': 'http://bitbucket.org/eventh/courses/downloads/Automated%20Generation%20of%20Protocol%20Dissectors%20for%20Wireshark.pdf',
+     'date': 'November 2011'},
+
+    {'title': 'Topic 5: Evaluating OSS projects',
+     'description': 'Presentation given in TDT10 New Software Technology: COTS and Open Source Software at NTNU.',
+     'url': 'http://bitbucket.org/eventh/courses/downloads/Evaluating_OSS_projects_presentation.pdf',
+     'date': 'September 2011'},
+
+    {'title': 'Project Report: Web-intelligence',
+     'description': 'Project report from mandatory assignment in TDT4215 Web-intelligence at NTNU.',
+     'url': 'http://bitbucket.org/eventh/courses/downloads/TDT4215_report.pdf',
+     'date': 'March 2012'},
+
+    {'title': 'Project Presentation: Web-intelligence',
+     'description': 'Presentation given about project in TDT4215 Web-intelligence at NTNU.',
+     'url': 'http://bitbucket.org/eventh/courses/downloads/TDT4215_presentation.pdf',
+     'date': 'April 2012'},
 ]
 
 
@@ -24,9 +51,10 @@ def documents(request):
     # Add static_url to filenames to create url
     for doc in docs:
         if 'url' not in doc:
-            doc['url'] = '%sfiles/%s' % (settings.MEDIA_URL, doc['file'])
+            doc['url'] = '%sfiles/%s' % (settings.STATIC_URL, doc['file'])
 
-    context = {'count': len(docs), 'docs': docs, 'url': ''}
+    context = {'count': len(docs), 'docs': docs,
+               'url': 'http://bitbucket.org/eventh/courses/downloads'}
 
     return HttpResponse(json.dumps(context),
                         'application/json; charset=utf-8')
